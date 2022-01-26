e23f882d02379bc43bdf11dc70a6b541de273489
kherma <kamil.herma01@gmail.com>
2022-01-26 21:19:17 +0100

    Add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  background-color: pink;
+  color: white;
+  font-size: 3rem;
+`;
 
 const Home: NextPage = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center'>
-      <h1 className='text-5xl bg-black text-white'>Hello Next</h1>
+    <div className='w-screen h-screen flex items-center justify-center flex-col gap-4'>
+      <h1 className='text-5xl bg-black text-white'>Hello Tailwind</h1>
+      <Title>Hello Styled-component</Title>
     </div>
   );
 };
