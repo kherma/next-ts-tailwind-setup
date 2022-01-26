d23ae0a564476a5b870ea4079b324e22e2a30d3c
kherma <kamil.herma01@gmail.com>
2022-01-26 21:46:52 +0100

    Add twin.macro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,38 @@
 import type { NextPage } from 'next';
-import styled from 'styled-components';
+import tw, { styled } from 'twin.macro';
 
-const Title = styled.h1`
-  background-color: pink;
-  color: white;
+const StyledTitle = styled.h1`
+  color: red;
+  background-color: blue;
   font-size: 3rem;
+  padding: 0;
+`;
+
+const TailwindStylesTitle = tw.h1`
+  bg-green-500 text-yellow-500 text-5xl
+`;
+
+const ConditionalTW = styled.div<{ isRed: boolean }>`
+  ${tw`text-white
+    font-bold
+    py-2
+    px-4
+    border
+    border-black
+    rounded`}
+  ${({ isRed }) =>
+    isRed
+      ? tw`bg-red-500 hover:bg-red-700`
+      : tw`bg-blue-500 hover:bg-blue-500`};
 `;
 
 const Home: NextPage = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center flex-col gap-4'>
       <h1 className='text-5xl bg-black text-white'>Hello Tailwind</h1>
-      <Title>Hello Styled-component</Title>
+      <StyledTitle>Hello Styled-component</StyledTitle>
+      <TailwindStylesTitle>Hello Twin Macro</TailwindStylesTitle>
+      <ConditionalTW isRed={true}>Conticional TW</ConditionalTW>
     </div>
   );
 };
